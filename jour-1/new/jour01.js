cc2713c99cb0d624bc867410422cf488c8472cfb
Kar1004 <abderkaramoko@gmail.com>
2022-01-25 15:54:26 +0100

    first commit

---
+++ jour-1/new/jour01.js
@@ -0,0 +1,42 @@
+console.log("hello world!");
+
+
+// STRING
+var test="mon nom est Al";
+console.log(test);
+
+//CONCATENATION
+
+var name ="Al"
+console.log("Nice to meet you" +" "+name)
+
+//String Length
+var testlenght="I'm very long !" ;
+console.log(testlenght.length);
+
+//Replace
+
+var food="croissant is meh" ;
+food=food.replace("meh"," so good");
+console.log(food);
+
+//Up and Down
+
+var basic="This is Cool";
+var basicUp= basic.toUpperCase();
+var basicDown=basic.toLowerCase();
+console.log(basicUp);
+console.log(basicDown);
+
+//split
+
+var word="banana";
+var letter=word.split("");
+console.log(letter);
+
+//template
+
+var age= 23;
+var template= `I m ${age} years old`;
+console.log(template);
+
